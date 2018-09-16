bc42bfa5c0e4b169635c502238293755cd2f6f40
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-16 16:42:11 -0500

    fields, properties, constructor, and methods in place for the Manager file. test run in program.cs successful.

--- CsharpPayroll/Manager.cs
+++ CsharpPayroll/Manager.cs
@@ -1,10 +1,32 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace CsharpPayroll
-//{
-//    class Manager : Staff
-//    {
-//    }
-//}
+namespace CsharpPayroll
+{
+    class Manager : Staff
+    {
+        private const float managerHourlyRate = 50;
+
+        public int Allowance { get; private set; }
+
+        public Manager(string name) : base(name, managerHourlyRate) { }
+
+        public override void CalculatePayment()
+        {
+            base.CalculatePayment();
+            Allowance = 0;
+
+            if (HoursWorked > 160)
+            {
+                Allowance = 1000;
+                TotalPay = BasicPay + Allowance;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "\nNameOfStaff = " + NameOfStaff + "\nManagerHourlyRate = " + managerHourlyRate + "\nHoursWorked = " + HoursWorked + "\nBasicPay = " + BasicPay + "\nAllowance = " + Allowance + "\nTotalPay = " + TotalPay;
+        }
+    }
+}
--- CsharpPayroll/Program.cs
+++ CsharpPayroll/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CsharpPayroll;
+using System;
+using System.Collections.Generic;
 
 namespace CSProject
 {
@@ -8,6 +10,13 @@ namespace CSProject
         {
             Console.WriteLine("Oh hey, hi");
 
+            Staff staff = new Staff("Bob", 50);
+            staff.CalculatePayment();
+            Console.WriteLine(staff.ToString());
+
+            Manager mgr1 = new Manager("natz");
+            mgr1.CalculatePayment();
+            Console.WriteLine(mgr1.ToString());
 
             Console.Read();
         }
