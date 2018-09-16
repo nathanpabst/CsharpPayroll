95a62df9153029e4d1b0d80e7f8b5ce8ba67887a
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-16 15:40:19 -0500

    fields, properties, constructor, and methods in place for the Staff file

--- CsharpPayroll/Admin.cs
+++ CsharpPayroll/Admin.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CsharpPayroll
-{
-    class Admin
-    {
-    }
-}
+//namespace CsharpPayroll
+//{
+//    //class Admin : Staff
+//    {
+
+//    }
+//}
--- CsharpPayroll/Staff.cs
+++ CsharpPayroll/Staff.cs
@@ -6,5 +6,49 @@ namespace CsharpPayroll
 {
     class Staff
     {
+        private float hourlyRate;
+        private int hWorked;
+
+        public float TotalPay { get; protected set; }
+        public float BasicPay { get; private set; }
+        public string NameOfStaff { get; private set; }
+
+        public int HoursWorked
+        {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (value > 0)
+                    hWorked = value;
+                else
+                    hWorked = 0;
+            }
+        }
+        public Staff(string name, float rate)
+        {
+            NameOfStaff = name;
+            hourlyRate = rate;
+
+        }
+
+        public virtual void CalculatePayment()
+        {
+            Console.WriteLine("Calculating Pay...");
+
+            BasicPay = hWorked * hourlyRate;
+            TotalPay = BasicPay;
+        }
+
+        public override string ToString()
+        {
+            return "\nNameOfStaff = " + NameOfStaff + "\nhourlyRate = " + hourlyRate + "\nWorked = " + hWorked + "\nBasicPay = " + BasicPay + "\n\nTotalPay = " + TotalPay;
+
+        }
+
     }
+
+    
 }
